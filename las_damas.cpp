4f0c6cf13d0d8247715e569d51663dfe6db9910a
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2022-05-27 11:53:45 -0300

    Tablero fachero re piola

--- las_damas.cpp
+++ las_damas.cpp
@@ -1,35 +1,35 @@
 #include<iostream>
 using namespace std;
 
-
-void printTable (int matrix[10][10]);
+void printTable (int matrix[10][10],bool isFirstPlayerTurn);
 void preloadTable (int matrix[10][10]);
 void moveFile (int matrix[10][10]);
 
-int main(int argc, char** argv) {
+int main() {
 	bool isFirstPlayerTurn = true;
+	bool isWinner = false;
 	int table[10][10] = {0};
-	
-	if(isFirstPlayerTurn)
-		cout << "Turno del jugador 1 \n";
-	else
-		cout << "Turno del jugador 2 \n";
-		
 	preloadTable(table);
-	printTable(table);
+	do{
+	printTable(table, isFirstPlayerTurn);
 	moveFile(table);
-	printTable(table);
-
+	isFirstPlayerTurn = !isFirstPlayerTurn;
+	}while(!isWinner);
 	return 0;
 }
 
-void printTable (int matrix[10][10]){
+void printTable (int matrix[10][10], bool isFirstPlayerTurn){
 	system("cls");
-	cout << "------------------------------\n";
-	cout << " 0  1  2  3  4  5  6  7  8  9 \n";
-	cout << "------------------------------\n";
+	isFirstPlayerTurn ? cout << "Turno del Jugador N 1 \n" : cout << "Turno del Jugador N 2\n";
+	cout << 
+	" +-----------------------------------+\n"
+	" | \\ X  0  1  2  3  4  5  6  7  8  9 |\n"
+	" | Y +-------------------------------+\n";
+
 	for(int i = 0; i < 10; i++){
 		for(int j = 0; j < 10; j++){
+			if( j == 0)
+			cout << " | " << i << " | ";
 			if((i + j) % 2 == 0)
 				cout << " x " ;
 			else
@@ -37,11 +37,11 @@ void printTable (int matrix[10][10]){
 		}	
 		cout << endl;
 	}
-	
 }
 
 void preloadTable (int matrix[10][10]){
 	
+	//LLena las fichas del jugador 1
 	for(int i = 0; i < 3; i++){
 		for(int j = 0; j < 10; j++){
 			if((i + j) % 2 == 1)
@@ -49,6 +49,7 @@ void preloadTable (int matrix[10][10]){
 		}	
 	}
 	
+	//Llena las fichas del jugador 2
 	for(int i = 7; i < 10; i++){
 		for(int j = 0; j < 10; j++){
 			if((i + j) % 2 == 1)
