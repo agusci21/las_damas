46a29b9f9f9617d76980d9c93277097a07222506
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2022-05-27 11:09:39 -0300

    Mejora en el movimientro de las fichas basado en ejes cartecianos

--- las_damas.cpp
+++ las_damas.cpp
@@ -1,55 +1,7 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
-void printTable (int matrix[10][10]);
-void preloadTable (int matrix[10][10]);
-
-int main(int argc, char** argv) {
-	int table[10][10] = {0};
-	int i,j;
-	preloadTable(table);
-	printTable(table);
-	
-	return 0;
-}
-
-void printTable (int matrix[10][10]){
-	
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			if((i + j) % 2 == 0)
-				cout << " x " ;
-			else
-			  cout << " "<< matrix[i][j] << " ";
-		}	
-		cout << endl;
-	}
-	
-}
-
-void preloadTable (int matrix[10][10]){
-	
-	for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 10; j++){
-			if((i + j) % 2 == 1)
-			  matrix[i][j] = 1;
-		}	
-	}
-	
-	for(int i = 7; i < 10; i++){
-		for(int j = 0; j < 10; j++){
-			if((i + j) % 2 == 1)
-			  matrix[i][j] = 2;
-		}	
-	}
-	
-}
 
 
-------------------------------------------------------------------------------------------
-
-
-#include<iostream> 
-using namespace std;
 void printTable (int matrix[10][10]);
 void preloadTable (int matrix[10][10]);
 void moveFile (int matrix[10][10]);
@@ -72,7 +24,10 @@ int main(int argc, char** argv) {
 }
 
 void printTable (int matrix[10][10]){
-	
+	system("cls");
+	cout << "------------------------------\n";
+	cout << " 0  1  2  3  4  5  6  7  8  9 \n";
+	cout << "------------------------------\n";
 	for(int i = 0; i < 10; i++){
 		for(int j = 0; j < 10; j++){
 			if((i + j) % 2 == 0)
@@ -106,9 +61,9 @@ void preloadTable (int matrix[10][10]){
 void moveFile (int matrix[10][10]){
 	int i,j;
 	char direction;
-	cout << "Que ficha quieres mover? ingrese posicion i, j \n";
+	cout << "Que ficha quieres mover? ingrese posicion x, y \n";
 	cin >> i >> j;
-	matrix[i][j] = 0;
+	matrix[j][i] = 0;
 	cout << "Movera a la derecha <D> o a la Izquierda <I>? \n";
 	cin >> direction;
 	if(direction == 'D'){
